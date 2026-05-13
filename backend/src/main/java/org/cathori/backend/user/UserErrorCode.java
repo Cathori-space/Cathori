1f14e9de6d6638402f21a7228abb4d9b482ff3b5
dlrlq <dlrlqor256@naver.com>
2026-05-13 12:28:39 +0900

    chore: 의존성경로 추가 및 사용자에러코드 수정
    - STMP의존성 및 passwordEncoder의존성 추가
    - 사용자 get함수 리턴값 수정

--- backend/src/main/java/org/cathori/backend/user/UserErrorCode.java
+++ backend/src/main/java/org/cathori/backend/user/UserErrorCode.java
@@ -23,16 +23,16 @@ public enum UserErrorCode implements ErrorCode {
 
     @Override
     public HttpStatus getStatus() {
-        return null;
+        return status;
     }
 
     @Override
     public String getCode() {
-        return "";
+        return code;
     }
 
     @Override
     public String getMessage() {
-        return "";
+        return message;
     }
 }
